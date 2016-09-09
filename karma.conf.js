42ee3b9c6f19e8d4b92cabe1254560722db95ca0
Wung <wungmathing@gmail.com>
2016-09-09 06:51:41 +0530

    Testing an Angular Factory and a Real API Endpoint

--- karma.conf.js
+++ karma.conf.js
@@ -19,9 +19,11 @@ module.exports = function(config) {
       './node_modules/angular-ui-router/release/angular-ui-router.js', // ui-router
       './node_modules/angular-mocks/angular-mocks.js',                 // loads our modules for tests
       './app/services/users/users.js',                                 // our Users factory
+      './app/services/pokemon/pokemon.js',
       './app/components/users/users.js',
       './app/app.js',                                                  // our angular app
       './app/services/users/users.spec.js',
+      './app/services/pokemon/pokemon.spec.js',
       './app/components/users/users.spec.js'
     ],
 
